fdf2d56967276c8bbc05d00d4aaf796603c76eee
DarioWeb <93431365+DarioWeb@users.noreply.github.com>
2022-02-14 16:21:48 +0100

    Update db.php

--- database/db.php
+++ database/db.php
@@ -1,1 +1,16 @@
+<?php
 
+class db
+{
+    protected function connect(){
+        try {
+            $username = "root";
+            $password = "";
+            $db = new PDO("mysql:dbname=stankovski;host=localhost",$username,$password);
+            return $db;
+        }
+        catch (PDOException $e){
+            die("COnnection error: ".$e);
+        }
+    }
+}
