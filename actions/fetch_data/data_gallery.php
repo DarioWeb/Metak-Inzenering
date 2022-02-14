e2615b42401c8c6c454e428e115f04f7a28e9e44
DarioWeb <93431365+DarioWeb@users.noreply.github.com>
2022-02-14 16:30:23 +0100

    Add files via upload

--- actions/fetch_data/data_gallery.php
+++ actions/fetch_data/data_gallery.php
@@ -1,1 +1,20 @@
+<?php
+include "database/db.php";
 
+class data_gallery extends db
+{
+
+    public function gallery($where){
+
+        $sql = $this->connect()->prepare("SELECT * FROM uploads WHERE type = ?");
+        if (!$sql->execute(array($where))){
+            die("Error");
+        }
+        if ($sql->rowCount() > 0){
+            return $sql;
+        }else{
+            return false;
+        }
+    }
+
+}
